b68da18793af64ac8345cb7d167854c0f90fa43f
jan@t460-U16 <jan.debiec@ue-mail.de>
2017-12-27 10:17:53 +0100

    added limit to binary search

--- find_line_tsv.py
+++ find_line_tsv.py
@@ -1,7 +1,29 @@
 import helper
+from argparse import ArgumentParser
 
 file = 'imdbif/title_basics.tsv'
 
-line = helper.findLineWithId(file, '3397884')
+
+
+
+
+parser = ArgumentParser()
+
+# Add more options if you like
+parser.add_argument("-f", "--file", dest="filenameVariable",
+                    help="write report to FILE", metavar="FILE")
+parser.add_argument("-i", "--id", dest="valueId",
+                    help="ID of movie/person in IMDb", metavar="IMDBID")
+parser.add_argument("-q", "--quiet",
+                    action="store_false", dest="verbose", default=True,
+                    help="don't print status messages to stdout")
+
+args = parser.parse_args()
+
+print(args.filenameVariable)
+print(args.filenameVariable)
+
+line = helper.findLineWithId(args.filenameVariable, args.valueId)
+# line = helper.findLineWithId(file, '3397885')
 
 print(line)
--- helper/__init__.py
+++ helper/__init__.py
@@ -36,11 +36,14 @@ def csvToDbTable(csvFile, delimiter):
 
 def findLineWithId(filename, matchId, delimiter='\t'):
     '''binary search in TSV files from IMDB'''
+    counter = 0
     start = 0
     end = os.path.getsize(filename)
+    endBinary = "{0:b}".format(end)
+    maxLoopCount = len(endBinary)
     with ManagedFile(filename) as fptr:
 
-        while (start < end):
+        while (start < end) and (counter < maxLoopCount):
             pos = start + ((end - start) / 2)
 
             fptr.seek(pos)
@@ -49,6 +52,8 @@ def findLineWithId(filename, matchId, delimiter='\t'):
             values = line.split(sep=delimiter)
             firstValue = values[0]
             lineId = firstValue[2:]# ignore the first 2 chars
+            print(lineId)
+            counter = counter + 1
             if matchId == lineId:
                 return line
             elif matchId > lineId:
