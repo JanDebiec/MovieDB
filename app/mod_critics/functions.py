a703bff5abfc62aef30048497f114dd17761d76a
jan@t460-U16 <jan.debiec@ue-mail.de>
2018-01-27 22:45:14 +0100

    implementing person

--- app/mod_critics/functions.py
+++ app/mod_critics/functions.py
@@ -15,6 +15,7 @@ class Comparison():
         self.countA = 0
         self.ratingsB = {}
         self.countB = 0
+        # self.person = 0
 
     def findCommonRatings(self):
         ''' search through whole DB
@@ -74,6 +75,58 @@ class Comparison():
     #     3. calc
     #     4. normalize to movie count'''
     #     self.findCommonRatings()
+
+
+    # Returns the Pearson correlation coefficient for p1 and p2
+    def sim_pearson(self):
+    # def sim_pearson(prefs, p1, p2):
+    #     # Get the list of mutually rated items
+    #     si = {}
+    #     for item in prefs[p1]:
+    #         if item in prefs[p2]: si[item] = 1
+    #
+    #     # if they are no ratings in common, return 0
+    #     if len(si) == 0: return 0
+    #
+        # Sum calculations
+        n = len(self.sharedRatings)
+
+        # Sums of all the preferences
+        sum1 = 0
+        sum2 = 0
+        sum1Sq = 0
+        sum2Sq = 0
+        pSum = 0
+        for item in self.sharedRatings:
+            rata = item[0]
+            ratb = item[1]
+            sum1 = sum1 + rata
+            sum2 = sum2 + ratb
+
+            # sum1 = sum([prefs[p1][it] for it in si])
+            # sum2 = sum([prefs[p2][it] for it in si])
+
+            # Sums of the squares
+            sum1Sq = sum1Sq + pow(rata, 2)
+            sum2Sq = sum2Sq + pow(ratb, 2)
+            # sum1Sq = sum([pow(prefs[p1][it], 2) for it in si])
+            # sum2Sq = sum([pow(prefs[p2][it], 2) for it in si])
+
+            # Sum of the products
+            pSum = pSum + (rata * ratB)
+            # pSum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+
+        # Calculate r (Pearson score)
+        num = pSum - (sum1 * sum2 / n)
+        den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+        self.person = 0
+        if den == 0:
+            return # person
+
+        self.person = num / den
+
+        # return person
+
 
 def getRatingsFromCritic(criticName):
     ''' return the list of ratings for the critic '''
