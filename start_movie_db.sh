24f91ec9069d439718cdbc75fd07365e995b87e0
jan@t460-U16 <jan.debiec@ue-mail.de>
2018-02-03 19:18:40 +0100

    fixing autostart.. ..

--- start_movie_db.sh
+++ start_movie_db.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # a script to start movieDB
 
-cd /home/jan/project/movie_db/
-. /home/jan/project/movie_db/venv/bin/activate
-#echo $FLASK_APP
+cd /home/ubuntu/project/MovieDb/
+. /home/ubuntu/project/MovieDb/venv/bin/activate
+echo $FLASK_APP
 flask run
 
