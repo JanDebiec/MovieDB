a07705f5fd6dfb2a4f0f2d73b8911bc17067daca
jan@t460-U16 <jan.debiec@ue-mail.de>
2018-02-03 19:07:16 +0100

    fixing autobackup (excluded more folders)

--- auto_backup.sh
+++ auto_backup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/ubuntu/project
-sudo rsync -rv --exclude 'venv/' MovieDB/ admin@192.168.178.197:/volume1/MovieDB/
+sudo rsync -rv --exclude 'venv/' --exclude 'imdbif/' MovieDB/ admin@192.168.178.197:/volume1/MovieDB/
