9827faa13d94a11cd66d6dc154c0aa6fbf60556b
jan@t460-U16 <jan.debiec@ue-mail.de>
2018-03-26 09:11:09 +0200

    changed loging, alwaus to file

--- app/__init__.py
+++ app/__init__.py
@@ -52,24 +52,37 @@ def create_app(config_class=Config):
 
     app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
-    if not app.debug and not app.testing:
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/moviedb.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
+    # if not app.debug and not app.testing:
+    #     if app.config['LOG_TO_STDOUT']:
+    #         stream_handler = logging.StreamHandler()
+    #         stream_handler.setLevel(logging.INFO)
+    #         app.logger.addHandler(stream_handler)
+    #     else:
+    #         if not os.path.exists('logs'):
+    #             os.mkdir('logs')
+    #         file_handler = RotatingFileHandler('logs/moviedb.log',
+    #                                            maxBytes=10240, backupCount=10)
+    #         file_handler.setFormatter(logging.Formatter(
+    #             '%(asctime)s %(levelname)s: %(message)s '
+    #             '[in %(pathname)s:%(lineno)d]'))
+    #         file_handler.setLevel(logging.INFO)
+    #         app.logger.addHandler(file_handler)
+    #
+    #     app.logger.setLevel(logging.INFO)
+    #     app.logger.info('MovieDB startup')
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('MovieDB startup')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/moviedb.log',
+                                       maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('MovieDB startup')
 
     return app
 
