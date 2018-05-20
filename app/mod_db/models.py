80f907f7387d208247a8ee55df5a1e2519019ec3
jan@t460-U16 <jan.debiec@ue-mail.de>
2018-05-20 20:14:33 +0200

    start with Model for User

--- app/mod_db/models.py
+++ app/mod_db/models.py
@@ -1,4 +1,7 @@
 from app import db
+from flask_login import UserMixin
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # Define a base model for other database tables to inherit
@@ -119,3 +122,18 @@ class Rating(DbBase):
 
     def __repr__(self):
         return '<Rating movie={} critic={} Val={}'.format(self.movie_id, self.critic_id, self.value)
+
+
+class User(UserMixin, DbBase):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
